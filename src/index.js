a48a6bd143e9b66249e35411eb913c0415222eba
Blshop <envworking@gmail.com>
2021-08-22 16:50:15 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = n.toString()
+  let newString = ''
+  n = n.replace('-','')
+  for (let i=0; i<n.length; i++){
+      newString +=n[n.length-i-1]
+  }
+  return newString
 }
